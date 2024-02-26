b864054a5134e8c6430c987ce5258fb426933ac9
SelvinVasquez <svasquezl9@miumg.edu.gt>
2024-02-26 16:13:03 -0600

    SelvinVasquezPrueba

--- main.cpp
+++ main.cpp
@@ -17,35 +17,3 @@ int main(){
     }
 
 }
-
-/*#include <iostream>
-using namespace std;
-
-    int main(){
-        int numero, i=0;
-        cout<<"Ingrese un numero";
-        cin>>numero;
-        while (i <= numero){
-            if(i % 2 ==0){0
-                cout<<i<<endl;
-            }
-            i++;
-        }
-        return 0;
-        }
-#include <iostream>
-using namespace std;
-int main() {
-    int n;
-    cout << "Ingrese la cantidad de variables que desea ver ";
-    cin >> n;
-    int suma = 0;
-    for (int i = 1; i <= n; i++) {
-        suma += i;
-        cout << suma << ", ";
-    }
-
-    cout << "..." <<endl;
-
-    return 0;
-}*/
